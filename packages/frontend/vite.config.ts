e87267e423605d7787c15dbebb046123e02d3e1f
kaylas000 <kaylas000@yandex.com>
2025-12-08 12:37:26 +0300

    Config: Vite configuration for React + Phaser

---
+++ packages/frontend/vite.config.ts
@@ -0,0 +1,37 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@scenes': path.resolve(__dirname, './src/scenes'),
+      '@services': path.resolve(__dirname, './src/services'),
+      '@store': path.resolve(__dirname, './src/store'),
+      '@types': path.resolve(__dirname, './src/types'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+    },
+  },
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+  build: {
+    target: 'ES2020',
+    outDir: 'dist',
+    sourcemap: true,
+    minify: 'terser',
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom', 'phaser', 'zustand', 'socket.io-client'],
+  },
+});
