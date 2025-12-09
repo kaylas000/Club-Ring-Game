56ffd4512d1929f52f0d8f578007a60b38d7ee86
kaylas000 <kaylas000@yandex.com>
2025-12-09 13:46:10 +0300

    Update combat.module.ts to import TypeORM entities

--- backend/src/modules/combat/combat.module.ts
+++ backend/src/modules/combat/combat.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CombatGateway } from './combat.gateway';
 import { CombatService } from './combat.service';
+import { Player } from '../players/player.entity';
+import { Match } from '../matches/match.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Player, Match])],
   providers: [CombatGateway, CombatService],
   exports: [CombatService],
 })
